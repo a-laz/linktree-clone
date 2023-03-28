502cbd4cbae3a5a19bc015bf501aae501e9ef622
Alex Lazarev <mortaleusmarketing@gmail.com>
2023-03-28 15:14:11 -0400

    Added Users, styling to Devise pages, and added navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get 'dashboard', to: 'dashboard#index', as: 'dashboard'
+  root 'dashboard#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
