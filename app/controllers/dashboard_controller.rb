ab571fc68310268c638828a20b9c625ddaa1a635
Alex Lazarev <mortaleusmarketing@gmail.com>
2023-03-29 10:39:09 -0400

    added links to social pages

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -11,12 +11,18 @@ class DashboardController < ApplicationController
 
   def show
     redirect_to dashboard_path if @user.nil?
+    
+    @links = @user.links.where.not(url: '', title: '')
   end
 
   private
 
   def set_user
     # localhost:3000/1
-    @user = User.find_by_id(params[:id])
+    begin
+      @user = User.friendly.find(params[:id])
+    rescue StandardError
+      @user = nil
+    end
   end
 end
